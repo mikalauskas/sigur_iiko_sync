05cfb001768bbc3919569c3a5fd6e8fc295bd98d
mikalauskas <rusm25@gmail.com>
2024-02-16 16:01:19 +0500

    optimizing code, adding 1c feature

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ const iikoCategoryId = process.env.IIKO_STUDENT_CATEGORY;
 
 const sigur = new Sigur(sigurDbHost, SigurDbPort, SigurDbUser, SigurDbPassword, SigurDbName);
 
-const dumpToJsonFile = (filename, data) => {
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const dumpToJson = (filename, data) => {
     if (data) {
         console.log(`Writing data to ${filename}`);
         fs.writeFile(filename, "\ufeff" + JSON.stringify(data, null, 4).replace(/\n/g, "\r\n"), err => {
@@ -32,61 +34,79 @@ const dumpToJsonFile = (filename, data) => {
 // Compare cards of iiko users
 // Add a new card, remove old cards
 async function compareCards(token, orgId, fio, phone, customerId, userCards, newCard) {
-    iiko.addCard(token, orgId, customerId, newCard);
-    for (let i in userCards) {
-        if (userCards[i].number !== newCard) {
-            console.log(`User: ${fio} (${phone}) current card: ${userCards[i].number}, newcard: ${newCard}`);
-            iiko.removeCard(token, orgId, customerId, userCards[i].number);
+    await delay(300);
+    await iiko.addCard(token, orgId, customerId, newCard);
+    //console.log(`User: ${fio} (${phone}) newCard: ${newCard}`);
 
-        }
+    const cardsToRemove = userCards
+        .filter(card => card.number !== newCard)
+        .map(card => card.number);
+
+    if (cardsToRemove.length > 0) {
+        console.log('cards to remove:');
+        console.log(cardsToRemove);
+    }
+
+    for (let cardNumber of cardsToRemove) {
+        console.log(`User: ${fio} (${phone}) removing card: ${cardNumber}`);
+        await delay(300);
+        await iiko.removeCard(token, orgId, customerId, cardNumber);
     }
 }
 
 // Compare Sigur users and Umed users
 async function compareUsers(umedUsers, sigurUsers, token, orgId) {
     console.log('Comparing job started');
-    let foundUsersinIiko = [];
-    let fioUmed,
-        fioSigur,
-        firstNameUmed,
-        lastNameUmed,
-        secondNameUmed,
-        loginUmed,
-        codeKeySigur = undefined;
+    const foundUsersinIiko = [];
 
-    for (let i in umedUsers) {
-        firstNameUmed = umedUsers[i]['NAME'];
-        lastNameUmed = umedUsers[i]['LAST_NAME'];
-        secondNameUmed = umedUsers[i]['SECOND_NAME'];
-        fioUmed = lastNameUmed + ' ' + firstNameUmed + ' ' + secondNameUmed;
-        loginUmed = umedUsers[i]['LOGIN'];
-        for (let j in sigurUsers) {
-            fioSigur = sigurUsers[j]['NAME'];
-            codeKeySigur = sigurUsers[j]['CODEKEY'];
-            let compare = stringSimilarity(fioUmed, fioSigur)
-            if (compare > 0.95) {
+    // Iterate through each user from umedUsers
+    for (const umedUser of umedUsers) {
+        const { NAME: firstNameUmed, LAST_NAME: lastNameUmed, SECOND_NAME: secondNameUmed, LOGIN: loginUmed } = umedUser;
+        const fioUmed = `${lastNameUmed} ${firstNameUmed} ${secondNameUmed}`;
+
+        // Iterate through each user from sigurUsers
+        for (const sigurUser of sigurUsers) {
+            const { NAME: fioSigur, CODEKEY: codeKeySigur } = sigurUser;
+
+            // Calculate similarity between names
+            const similarity = stringSimilarity(fioUmed, fioSigur);
+
+            // If similarity is greater than 0.95, consider it a match
+            if (similarity > 0.95) {
                 // Search user in iiko
-                let response = await iiko.getCustomerInfo(token, orgId, loginUmed);
+                await delay(300);
+                const response = await iiko.getCustomerInfo(token, orgId, loginUmed);
+
+                // If user not found in iiko, create and add to category
                 if (response === 400) {
-                    // Create user in iiko
                     console.log(`Creating user ${loginUmed}`);
-                    let customerId = await iiko.createUser(token, orgId, loginUmed, firstNameUmed, lastNameUmed, secondNameUmed, codeKeySigur);
+                    await delay(300);
+                    const customerId = await iiko.createUser(token, orgId, loginUmed, firstNameUmed, lastNameUmed, secondNameUmed, codeKeySigur);
+
                     if (customerId) {
-                        console.log(`Created user with id: ${customerId['id']}`);
+                        console.log(`Created user with id: ${customerId.id}`);
                     }
+
                     console.log(`Adding user ${loginUmed} to student category`);
-                    let userInCategory = await iiko.addUserToCategory(token, orgId, customerId['id'], iikoCategoryId);
-                    if (userInCategory) console.log(`User ${loginUmed} added to category`);
+                    await delay(300);
+                    const userInCategory = await iiko.addUserToCategory(token, orgId, customerId.id, iikoCategoryId);
+
+                    if (userInCategory) {
+                        console.log(`User ${loginUmed} added to category`);
+                    }
+
                     continue;
                 }
 
-                // compare cards and add/remove
+                // If user found in iiko, compare cards and add/remove
                 compareCards(token, orgId, fioUmed, loginUmed, response.id, response.cards, codeKeySigur);
                 foundUsersinIiko.push(response);
+                
                 continue;
             }
         }
     }
+
     console.log(`Found users in iiko db: ${foundUsersinIiko.length}`);
     return foundUsersinIiko;
 }
@@ -103,15 +123,15 @@ async function syncUsers(sigurUsers) {
         console.log('Comparing user between Sigur and Umed');
         const compareResult = await compareUsers(umedUsers, sigurUsers, token, orgId);
         console.log('Comparing job finished');
-        dumpToJsonFile('iikoUsers.json', compareResult);
+        dumpToJson('iikoUsers.json', compareResult);
     } catch (error) {
         console.log(error);
     }
 }
 
-sigur.getPersonal('%туден%', sigurUsers => {
+sigur.getPersonal(sigurUsers => {
     console.log(`Total users in sigur: ${sigurUsers.length}`);
     syncUsers(sigurUsers);
-    dumpToJsonFile('personal.json', sigurUsers);
+    dumpToJson('personal.json', sigurUsers);
 });
 
