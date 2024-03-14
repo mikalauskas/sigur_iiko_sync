77e48d612f7bcc923dab95781ca5e0ecf6b93129
Ruslan M <rusm25@gmail.com>
2024-03-14 22:00:12 +0500

    1cfresh ongoing development

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const utils = require('./lib/utils.js');
 const { syncUmed } = require('./lib/umed.js');
 const { syncMoodle } = require('./lib/moodle.js');
 const { create1cJsonData } = require('./lib/1c.js');
+const { create1cFreshJsonData } = require('./lib/1cFresh.js');
 require('log-timestamp');
 dotenv.config();
 
@@ -17,6 +18,9 @@ const umedToken = process.env.UMED_TOKEN;
 const iikoApi = process.env.IIKO_API;
 const iikoCategoryId = process.env.IIKO_STUDENT_CATEGORY;
 const moodleToken = process.env.MOODLE_TOKEN;
+const C1FreshUrl = process.env.C1_FRESH_ENDPOINT;
+const C1FreshLogin = process.env.C1_FRESH_LOGIN;
+const C1FreshPassword = process.env.C1_FRESH_PASSWORD;
 
 const getSigurUsers = async () => {
   const sigur = new Sigur(
@@ -43,6 +47,11 @@ const getSigurUsers = async () => {
 };
 
 (async () => {
+  /* const C1FreshUsers = await create1cFreshJsonData(
+    C1FreshUrl,
+    C1FreshLogin,
+    C1FreshPassword,
+  ); */
   const CUsers = await create1cJsonData();
 
   await syncUmed(umedToken, CUsers);
