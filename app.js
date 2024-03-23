465f3535856010294dd5277d93cecba4741f453a
mikalauskas <rusm25@gmail.com>
2024-03-23 23:00:58 +0500

    add sentry

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ const utils = require('./lib/utils.js');
 const { syncUmed } = require('./lib/umed.js');
 const { syncMoodle } = require('./lib/moodle.js');
 const { create1cJsonData } = require('./lib/1c.js');
+const Sentry = require('@sentry/node');
+const { nodeProfilingIntegration } = require('@sentry/profiling-node');
+
 require('log-timestamp');
 dotenv.config();
+
+const sentryDSN = process.env.sentryDSN;
+
+Sentry.init({
+  dsn: sentryDSN,
+  integrations: [nodeProfilingIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
 
 const getSigurUsers = async () => {
   const sigur = new Sigur();
